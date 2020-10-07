758fcedd3c98da92d9fc39e0ad633641e900ced3
Dominik Lameter <dominik@lameter.com>
2020-10-07 13:19:11 -0500

    Added routing for an index page.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let index = warp::path("index.html").or(warp::path::end()).map(|_| "This is the index page!");
+
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    warp::serve(hello)
+    let routes = warp::get().and(index.or(hello));
+
+    warp::serve(routes)
         .run(([127,0,0,1], 3030))
         .await;
 }
