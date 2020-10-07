d5158754e3a1f13fea7755e37bc107b6063aea55
Dominik Lameter <dominik@lameter.com>
2020-10-07 15:00:27 -0500

    Moved from a static line of text to reading in a file from the local file system. Moved from an or to a redirect on '/' as reading in the file with warp can't directly use an or.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use warp::Filter;
+use warp::{http::Uri, Filter};
 
 #[tokio::main]
 async fn main() {
-    let index = warp::path("index.html").or(warp::path::end()).map(|_| "This is the index page!");
+    let index = warp::path("index.html").and(warp::fs::file("index.html"));
+    let index_redirect = warp::path::end().map(|| warp::redirect(Uri::from_static("/index.html")));
 
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    let routes = warp::get().and(index.or(hello));
+    let routes = warp::get().and(index.or(index_redirect).or(hello));
 
     warp::serve(routes)
         .run(([127,0,0,1], 3030))
