8ec7bc0cb02ea2087ccf87f4919bdb87ac604e8e
Dominik Lameter <dominik@lameter.com>
2020-10-14 12:13:13 -0500

    Implemented function to handle file parsing to metadata, handling frontmatter along the way.

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,12 @@ async fn main() {
     println!("Test nested templating:\n{}", output);
 
     let test_str = "---\nlayout: base\ntitle: 'a page'\n---\n<p>This is the content of the webpage!</p>".to_string();
+    /*
     let test_split: Vec<&str> = test_str.splitn(3, "---\n").collect();
     println!("Testing splitn on:\n{}\nResult of splitn:\n{}\n{}\n{}", test_str, test_split[0], test_split[1], test_split[2]);
+    */
 
-    let test_metadata = process_file_frontmatter(test_split[1].to_string());
+    let test_metadata = get_file_metadata(&test_str);
     println!("Extracted metadata:");
     for (key, value) in &test_metadata {
         println!("{} = {}", key, value);
@@ -136,25 +138,29 @@ fn add_file_to_layout_collection(layouts: &mut HashMap<String, String>, file_pat
         Err(e) => return Err(format!("Failed to add file to layout collection due to: {}", e)),
     }
 }
+*/
 
-fn get_file_metadata(file_string: String) -> HashMap<String, String> {
+fn get_file_metadata(file_string: &String) -> HashMap<String, String> {
     if file_string.starts_with("---\n") {
         // Split on --- twice
-        let split: Vec<String> = file_string.splitn(3, "---\n").collect();
+        let split: Vec<&str> = file_string.splitn(3, "---\n").collect();
 
         // Confirm it was twice
         if split.len() != 3 {
-            let metadata = HashMap::new();
+            let mut metadata = HashMap::new();
             metadata.insert("content".to_string(), file_string.clone());
+
+            metadata
         }
+        else {
+            // Process first split portion for metadata
+            let mut metadata = process_file_frontmatter(split[1].to_string().clone());
 
-        // Process first split portion for metadata
-        let metadata = process_file_metadata(split[1].clone());
+            // Insert last bit as content
+            metadata.insert("content".to_string(), split[2].to_string().clone());
 
-        // Insert last bit as content
-        metadata.insert("content".to_string(), split[2].clone());
-
-        metadata
+            metadata
+        }
     }
     else {
         let mut metadata = HashMap::new();
@@ -163,7 +169,6 @@ fn get_file_metadata(file_string: String) -> HashMap<String, String> {
         metadata
     }
 }
-*/
 
 fn process_file_frontmatter(metadata_string: String) -> HashMap<String, String> {
     let mut metadata = HashMap::new();
@@ -176,7 +181,7 @@ fn process_file_frontmatter(metadata_string: String) -> HashMap<String, String> 
         let key = split[0].trim();
         if key.is_empty() { break; }
 
-        if (split.len() > 1) {
+        if split.len() > 1 {
             let value = split[1].trim();
             metadata.insert(key.to_string(), value.to_string());
         }
