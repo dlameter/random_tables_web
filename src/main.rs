56fc49fa8519562655a31654fd0f053af4073d99
Dominik Lameter <dominik@lameter.com>
2020-10-17 13:17:57 -0500

    Moved templating functions to a seperate module file, main file was getting to large.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use std::collections::HashMap;
-use std::io::prelude::*;
-use std::fs;
-use std::fs::{File};
-use std::ops::Deref;
 use std::path::Path;
 use std::sync::Arc;
 
@@ -10,9 +5,11 @@ use warp::{http::Uri, Filter};
 use liquid;
 use liquid::*;
 
+mod templating;
+
 #[tokio::main]
 async fn main() {
-    let compiler = construct_liquid_complier();
+    let compiler = templating::construct_liquid_complier();
 
     // Build parser
     let parser = ParserBuilder::with_stdlib()
@@ -41,13 +38,13 @@ async fn main() {
     println!("Testing splitn on:\n{}\nResult of splitn:\n{}\n{}\n{}", test_str, test_split[0], test_split[1], test_split[2]);
     */
 
-    let test_metadata = get_file_metadata(&test_str);
+    let test_metadata = templating::get_file_metadata(&test_str);
     println!("Extracted metadata:");
     for (key, value) in &test_metadata {
         println!("{} = {}", key, value);
     }
 
-    let layouts = create_layout_collection().unwrap();
+    let layouts = templating::create_layout_collection().unwrap();
     for (layout_name, layout_metadata) in layouts.iter() {
         println!("Metadata of layout {}:", layout_name);
         for (key, value) in layout_metadata {
@@ -59,7 +56,7 @@ async fn main() {
 
     
     // Actual program stuff
-    let template_file = move |file_path| render_file(parser.clone(), file_path);
+    let template_file = move |file_path| templating::render_file(parser.clone(), file_path);
 
     let string = "This is a test string to place into a filter";
     
@@ -78,165 +75,4 @@ async fn main() {
     warp::serve(routes)
         .run(([127,0,0,1], 3030))
         .await;
-}
-
-fn construct_liquid_complier() -> liquid::partials::EagerCompiler::<liquid::partials::InMemorySource> {
-    let mut compiler = liquid::partials::EagerCompiler::<liquid::partials::InMemorySource>::empty();
-
-    match add_files_to_compiler(&mut compiler) {
-        Ok(_) => (),
-        Err(_) => print!("Failed to add files to compiler.\n"),
-    }
-
-    compiler
-}
-
-fn add_files_to_compiler(compiler: &mut liquid::partials::EagerCompiler::<liquid::partials::InMemorySource>) -> std::io::Result<()> {
-    let dir = Path::new("./_includes/");
-
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            if path.is_file() {
-                let path_str = match path.to_str() {
-                    Some(s) => s,
-                    None => "[error: could not get str representation of path]",
-                };
-
-                match get_file_contents(&path) {
-                    Ok(contents) => {
-                        compiler.add(path_str, contents);
-                        ()
-                    },
-                    Err(e) => println!("{}", e),
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn create_layout_collection() -> std::io::Result<HashMap<String, HashMap<String, String>>> {
-    let mut layouts = HashMap::new();
-
-    let dir = Path::new("./_layout/");
-
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            match add_file_to_layout_collection(&mut layouts, &path) {
-                Ok(_) => (),
-                Err(e) => println!("Failed to add layout file to layout collection. Error text: {}", e),
-            }
-        }
-    }
-
-    Ok(layouts)
-}
-
-fn add_file_to_layout_collection(layouts: &mut HashMap<String, HashMap<String,String>>, file_path: &std::path::Path) -> Result<(), String> {
-    let file_stem: String = match file_path.file_stem().unwrap().to_os_string().into_string() {
-        Ok(s) => s,
-        Err(os) => return Err(format!("File stem is not a valid Unicode string. Lossy result is: {}", os.to_string_lossy())),
-    };
-
-    let contents = match get_file_contents(file_path) {
-        Ok(c) => c,
-        Err(e) => return Err(format!("Failed to add file to layout collection due to: {}", e)),
-    };
-
-    layouts.insert(file_stem, get_file_metadata(&contents));
-    
-    Ok(())
-}
-
-fn get_file_metadata(file_string: &String) -> HashMap<String, String> {
-    if file_string.starts_with("---\n") {
-        // Split on --- twice
-        let split: Vec<&str> = file_string.splitn(3, "---\n").collect();
-
-        // Confirm it was twice
-        if split.len() != 3 {
-            let mut metadata = HashMap::new();
-            metadata.insert("content".to_string(), file_string.clone());
-
-            metadata
-        }
-        else {
-            // Process first split portion for metadata
-            let mut metadata = process_file_frontmatter(split[1].to_string().clone());
-
-            // Insert last bit as content
-            metadata.insert("content".to_string(), split[2].to_string().clone());
-
-            metadata
-        }
-    }
-    else {
-        let mut metadata = HashMap::new();
-        metadata.insert("content".to_string(), file_string.clone());
-
-        metadata
-    }
-}
-
-fn process_file_frontmatter(metadata_string: String) -> HashMap<String, String> {
-    let mut metadata = HashMap::new();
-
-    for line in metadata_string.split("\n").collect::<Vec<&str>>() {
-        let line = line.to_string().trim().to_string();
-
-        let split: Vec<&str> = line.splitn(2, ":").collect();
-
-        let key = split[0].trim();
-        if key.is_empty() { break; }
-
-        if split.len() > 1 {
-            let value = split[1].trim();
-            metadata.insert(key.to_string(), value.to_string());
-        }
-        else {
-            metadata.insert(key.to_string(), "".to_string());
-        }
-    }
-
-    metadata
-}
-
-fn get_file_contents(path: &std::path::Path) -> Result<String, String> {
-    let path_str = match path.to_str() {
-        Some(s) => s,
-        None => "[error: could not get str representation of path]",
-    };
-
-    let mut file = match File::open(path) {
-        Ok(f) => f,
-        Err(_) => return Err(format!("Failed to open file: {}", path_str)),
-    };
-
-    let mut contents = String::new();
-
-    match file.read_to_string(&mut contents) {
-        Ok(_) => {
-            Ok(contents)
-        },
-        Err(_) => Err(format!("Failed to read the contents of file: {}", path_str)),
-    }
-}
-
-fn render_file(liquid_parser: Arc<liquid::Parser>, template_path: &std::path::Path/*, globals, &Object*/) -> impl warp::Reply {
-    let globals = object!({"empty": "empty?"});
-
-    let template = liquid_parser.parse_file(template_path)
-        .unwrap();
-
-    let output = template.render(&globals).unwrap();
-    println!("Test liquid templating:\n{}", output);
-
-    warp::reply::html(output)
 }
---
+++ src/templating.rs
@@ -0,0 +1,170 @@
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::fs;
+use std::fs::{File};
+use std::path::Path;
+use std::sync::Arc;
+
+use liquid;
+use liquid::*;
+
+pub fn construct_liquid_complier() -> liquid::partials::EagerCompiler::<liquid::partials::InMemorySource> {
+    let mut compiler = liquid::partials::EagerCompiler::<liquid::partials::InMemorySource>::empty();
+
+    match add_files_to_compiler(&mut compiler) {
+        Ok(_) => (),
+        Err(_) => print!("Failed to add files to compiler.\n"),
+    }
+
+    compiler
+}
+
+pub fn add_files_to_compiler(compiler: &mut liquid::partials::EagerCompiler::<liquid::partials::InMemorySource>) -> std::io::Result<()> {
+    let dir = Path::new("./_includes/");
+
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.is_file() {
+                let path_str = match path.to_str() {
+                    Some(s) => s,
+                    None => "[error: could not get str representation of path]",
+                };
+
+                match get_file_contents(&path) {
+                    Ok(contents) => {
+                        compiler.add(path_str, contents);
+                        ()
+                    },
+                    Err(e) => println!("{}", e),
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn create_layout_collection() -> std::io::Result<HashMap<String, HashMap<String, String>>> {
+    let mut layouts = HashMap::new();
+
+    let dir = Path::new("./_layout/");
+
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            match add_file_to_layout_collection(&mut layouts, &path) {
+                Ok(_) => (),
+                Err(e) => println!("Failed to add layout file to layout collection. Error text: {}", e),
+            }
+        }
+    }
+
+    Ok(layouts)
+}
+
+pub fn add_file_to_layout_collection(layouts: &mut HashMap<String, HashMap<String,String>>, file_path: &std::path::Path) -> Result<(), String> {
+    let file_stem: String = match file_path.file_stem().unwrap().to_os_string().into_string() {
+        Ok(s) => s,
+        Err(os) => return Err(format!("File stem is not a valid Unicode string. Lossy result is: {}", os.to_string_lossy())),
+    };
+
+    let contents = match get_file_contents(file_path) {
+        Ok(c) => c,
+        Err(e) => return Err(format!("Failed to add file to layout collection due to: {}", e)),
+    };
+
+    layouts.insert(file_stem, get_file_metadata(&contents));
+    
+    Ok(())
+}
+
+pub fn get_file_metadata(file_string: &String) -> HashMap<String, String> {
+    if file_string.starts_with("---\n") {
+        // Split on --- twice
+        let split: Vec<&str> = file_string.splitn(3, "---\n").collect();
+
+        // Confirm it was twice
+        if split.len() != 3 {
+            let mut metadata = HashMap::new();
+            metadata.insert("content".to_string(), file_string.clone());
+
+            metadata
+        }
+        else {
+            // Process first split portion for metadata
+            let mut metadata = process_file_frontmatter(split[1].to_string().clone());
+
+            // Insert last bit as content
+            metadata.insert("content".to_string(), split[2].to_string().clone());
+
+            metadata
+        }
+    }
+    else {
+        let mut metadata = HashMap::new();
+        metadata.insert("content".to_string(), file_string.clone());
+
+        metadata
+    }
+}
+
+pub fn process_file_frontmatter(metadata_string: String) -> HashMap<String, String> {
+    let mut metadata = HashMap::new();
+
+    for line in metadata_string.split("\n").collect::<Vec<&str>>() {
+        let line = line.to_string().trim().to_string();
+
+        let split: Vec<&str> = line.splitn(2, ":").collect();
+
+        let key = split[0].trim();
+        if key.is_empty() { break; }
+
+        if split.len() > 1 {
+            let value = split[1].trim();
+            metadata.insert(key.to_string(), value.to_string());
+        }
+        else {
+            metadata.insert(key.to_string(), "".to_string());
+        }
+    }
+
+    metadata
+}
+
+pub fn get_file_contents(path: &std::path::Path) -> Result<String, String> {
+    let path_str = match path.to_str() {
+        Some(s) => s,
+        None => "[error: could not get str representation of path]",
+    };
+
+    let mut file = match File::open(path) {
+        Ok(f) => f,
+        Err(_) => return Err(format!("Failed to open file: {}", path_str)),
+    };
+
+    let mut contents = String::new();
+
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {
+            Ok(contents)
+        },
+        Err(_) => Err(format!("Failed to read the contents of file: {}", path_str)),
+    }
+}
+
+pub fn render_file(liquid_parser: Arc<liquid::Parser>, template_path: &std::path::Path/*, globals, &Object*/) -> impl warp::Reply {
+    let globals = object!({"empty": "empty?"});
+
+    let template = liquid_parser.parse_file(template_path)
+        .unwrap();
+
+    let output = template.render(&globals).unwrap();
+    println!("Test liquid templating:\n{}", output);
+
+    warp::reply::html(output)
+}
