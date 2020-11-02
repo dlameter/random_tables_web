10260e5ff4c35d1424cda396495458dc11e66d96
Dominik Lameter <dominik@lameter.com>
2020-11-02 16:42:00 -0600

    Added database_handler function to deal with getting accounts from the database. Added example filter to warp server that allows that information to be shown in a browser.

--- src/database_handler.rs
+++ src/database_handler.rs
@@ -1,5 +1,11 @@
 use postgres::{Client, NoTls};
 
+use crate::data::account;
+
+const COLUMN_ID: &str = "account_id";
+const COLUMN_NAME: &str = "username";
+const COLUMN_PASSWORD: &str = "password_hash";
+
 pub struct DatabaseHandler {
     connection: postgres::Client
 }
@@ -16,11 +22,42 @@ impl DatabaseHandler {
         }
     }
 
-    pub fn create_user(&mut self, username: &String, password: &String) -> Result<(), String> {
+    pub fn create_account(&mut self, account: &account::Account) -> Result<(), String> {
         // TODO hash password before sending to database
-        match self.connection.execute("INSERT INTO account (username, password_hash) VALUES ($1, $2)", &[username, password]) {
+        match self.connection.execute("INSERT INTO account (username, password_hash) VALUES ($1, $2)", &[&account.name, &account.password]) {
             Ok(_) => Ok(()),
             Err(e) => return Err(format!("Failed to create user with error: {}", e)),
         }
+    }
+
+    pub fn find_account_by_id(&mut self, id: &i32) -> Option<account::Account> {
+        match self.connection.query_one("SELECT * FROM account WHERE account_id = $1", &[id]) {
+            Ok(row) => match DatabaseHandler::row_to_account(&row) {
+                Ok(account) => Some(account),
+                Err(_) => None,
+            },
+            Err(_) => None,
+        }
+    }
+    
+    fn row_to_account(row: &postgres::row::Row) -> Result<account::Account, String> {
+        let id: i32 = match row.try_get(COLUMN_ID) {
+            Ok(value) => value,
+            Err(error) => return Err(format!("Failed to get column {} with error: {}", COLUMN_ID, error)),
+        };
+        let name: String = match row.try_get(COLUMN_NAME) {
+            Ok(value) => value,
+            Err(error) => return Err(format!("Failed to get column {} with error: {}", COLUMN_NAME, error)),
+        };
+        let password: String = match row.try_get(COLUMN_PASSWORD) {
+            Ok(value) => value,
+            Err(error) => return Err(format!("Failed to get column {} with error: {}", COLUMN_PASSWORD, error)),
+        };
+
+        Ok(account::Account {
+            id,
+            name,
+            password,
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::sync::Arc;
+use std::sync::Mutex;
 
 use warp::{http::Uri, Filter};
 use liquid::object;
@@ -10,12 +11,26 @@ use templating::Templator;
 mod database_handler;
 use database_handler::DatabaseHandler;
 
+mod data;
+
 #[tokio::main]
 async fn main() {
     let handler = match DatabaseHandler::new() {
         Ok(c) => c,
         Err(e) => panic!(format!("Failed to create database handler with error: {}", e)),
     };
+    let handler = Arc::new(Mutex::new(handler));
+
+    let account_by_id = warp::path!("account" / i32)
+        .map(move |id| {
+            match handler.clone()
+                .lock()
+                .unwrap()
+                .find_account_by_id(&id) {
+                    Some(account) => format!("{:?}", account),
+                    None => format!("Could not find user with id {}", id),
+            }
+        });
 
     let templator = Templator::new("./_layout/".to_string(), "./_includes/".to_string());
     let templator = Arc::new(templator);
@@ -29,7 +44,7 @@ async fn main() {
 
     let static_files = warp::path("static").and(warp::fs::dir("static"));
 
-    let routes = warp::get().and(index.or(index_redirect).or(static_files));
+    let routes = warp::get().and(index.or(index_redirect).or(static_files).or(account_by_id));
 
     warp::serve(routes)
         .run(([127,0,0,1], 3030))
