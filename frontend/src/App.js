e371fa59ad0a0b061bc8a38dc98a10ddc8ba19c1
Dominik Lameter <dominik@lameter.com>
2020-12-05 08:38:07 -0600

    Removed default react project html from App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,28 +6,22 @@ import {
     useParams,
 } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
 import Home from './Home.js';
 import { AccountPage } from './Account.js';
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    React
-                    </a>
+            <div>
+                <header>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/account/1">First Account</Link>
+                        </li>
+                    </ul>
                 </header>
                 <Switch>
                     <Route exact path="/">
