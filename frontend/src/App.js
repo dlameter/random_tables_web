07864adf99fd564800c85759f2d1566cc3fb693d
Dominik Lameter <dominik@lameter.com>
2020-12-07 17:02:32 -0600

    Added to meta in index.html in public. Converted from raw HTML nav to material-ui example nav.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,33 +5,64 @@ import {
     Link,
     useParams,
 } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Home from './Home.js';
 import { AccountPage } from './Account.js';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+    const classes = useStyles();
+    
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/account/1">First Account</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/account/:accountId">
-                        <AccountPage />
-                    </Route>
-                </Switch>
-            </div>
+            <AppBar>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Random Tables Web
+                    </Typography>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/account/1">Account 1</Button>
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/account/1">First Account</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/account/:accountId">
+                    <AccountPage />
+                </Route>
+            </Switch>
         </Router>
     );
 }
