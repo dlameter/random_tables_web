d6725b0faaacda75ae34e923c831bbc1817b6a94
Dominik Lameter <dominik@lameter.com>
2020-12-08 19:46:58 -0600

    Converted the TableList to use material ui table components.

--- frontend/src/TableList.js
+++ frontend/src/TableList.js
@@ -1,19 +1,53 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 function TableList(props) {
+    const classes = useStyles();
+
     const tables = props.tables;
     let listItems = null;
-    if (tables != null) {
+    if (tables !== null) {
         listItems = tables.map((table) => 
-            <li>{table}</li>
+            <TableRow key={table}>
+                <TableCell component="th" scope="row">
+                    {table}
+                </TableCell>
+            </TableRow>
         );
     }
     else {
-        listItems = <li>No Tables Found.</li>
+        listItems = (
+            <TableRow>
+                <TableCell>No Tables Found.</TableCell>
+            </TableRow>
+        );
     }
+
     return (
-        <div>
-            <h2>List of random tables</h2>
-            <ul>{listItems}</ul>
-        </div>
+        <TableContainer component={Paper}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell><strong>Tables by Account</strong></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {listItems}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
 
