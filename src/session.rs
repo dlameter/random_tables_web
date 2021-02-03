57d1b5af4e153f8bcaec62a1ce11a67e08e6a058
Dominik Lameter <dominik@lameter.com>
2021-02-03 10:06:22 -0600

    Added authentication method to Session structs that creates a random session key.

--- src/session.rs
+++ src/session.rs
@@ -1,7 +1,10 @@
-use crate::data;
+use crate::data::account::Account;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use rand::distributions::Alphanumeric;
+use rand::thread_rng;
+use rand::Rng;
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
 pub type PooledPg = PooledConnection<ConnectionManager<PgConnection>>;
@@ -9,7 +12,7 @@ pub type PooledPg = PooledConnection<ConnectionManager<PgConnection>>;
 pub struct Session {
     connection: PooledPg,
     id: Option<i32>,
-    account: Option<data::account::Account>,
+    account: Option<Account>,
 }
 
 impl Session {
@@ -23,7 +26,7 @@ impl Session {
                     .inner_join(s::web_sessions)
                     .select((s::id, (a::id, a::username)))
                     .filter(s::cookie.eq(&key))
-                    .first::<(i32, data::account::Account)>(&conn)
+                    .first::<(i32, Account)>(&conn)
                     .ok()
             })
             .map(|(id, account)| (Some(id), Some(account)))
@@ -35,6 +38,41 @@ impl Session {
             account,
         }
     }
+
+    pub fn authenticate(&mut self, username: &str, password: &str) -> Option<String> {
+        if let Some(account) = Account::authenticate(&self.connection, username, password) {
+            // Create session key
+            let session_key = random_key(48);
+
+            // store info in DB
+            use crate::schema::web_sessions::dsl::*;
+            let result = diesel::insert_into(web_sessions)
+                .values((account_id.eq(account.id), cookie.eq(&session_key)))
+                .returning(id)
+                .get_results(&self.connection);
+
+            // store info in Session object
+            if let Ok([session_id]) = result.as_ref().map(|value| &**value) {
+                self.account = Some(account);
+                self.id = Some(*session_id);
+                return Some(session_key);
+            } else {
+                println!(
+                    "Failed to create session id for {}: {:?}",
+                    account.username, result
+                );
+            }
+        }
+        None
+    }
+}
+
+fn random_key(len: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
 }
 
 pub fn pg_pool(database_url: &str) -> Pool<ConnectionManager<PgConnection>> {
