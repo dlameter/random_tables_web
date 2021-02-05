e082668021619daa442b4238308dd6ae7f4afba0
Dominik Lameter <dominik@lameter.com>
2021-02-05 15:12:33 -0600

    Added use of auth at the homepage to display the users current username.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,18 @@
+import { useAuth } from "./auth";
+
 function Home() {
+    const auth = useAuth();
+
     return (
         <div>
-            <h1>Welcome to Random Tables Web!</h1>
+            <h1>
+                {
+                    auth.user ?
+                        "Welcome " + auth.user.username + " to Random Tables Web!"
+                        :
+                        "Welcome to Random Tables Web!"
+                }
+            </h1>
         </div>
     );
 }
