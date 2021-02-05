56f12a51a1b6888be2a433abfeb1b8d882da654e
Dominik Lameter <dominik@lameter.com>
2021-02-05 13:53:38 -0600

    Removed all mention of CookieTest from frontend.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,7 @@
-import CookieTest from './CookieForm';
-
 function Home() {
     return (
         <div>
             <h1>Welcome to Random Tables Web!</h1>
-            <CookieTest />
         </div>
     );
 }
