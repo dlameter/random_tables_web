a5168f121ba0ab413257137d6a8287150b52b15c
Dominik Lameter <dominik@lameter.com>
2021-02-22 17:02:14 -0600

    First draft of update account function created, it is a mess and needs a lot of refactoring.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde_json;
 use warp::{http::Response, Filter, Rejection};
 
 use random_tables_web;
+use random_tables_web::data;
 use random_tables_web::session;
 
 #[tokio::main]
@@ -38,6 +39,12 @@ async fn main() {
         .and(warp::path::end())
         .and(session_closure())
         .and_then(|session| async move { do_whois(session) });
+    let update_account = warp::put()
+        .and(warp::path!("account" / i32))
+        .and(warp::path::end())
+        .and(session_closure())
+        .and(warp::body::json())
+        .and_then(|id, session, edit_data| async move { edit_account(session, id, edit_data) });
 
     let auth = login.or(logout).or(signup).or(whois);
 
@@ -143,6 +150,114 @@ fn do_whois(session: session::Session) -> Result<Response<String>, Rejection> {
                     .status(warp::http::StatusCode::OK)
                     .body(string)
                     .map_err(|error| warp::reject());
+            }
+        }
+    }
+    Response::builder()
+        .status(warp::http::StatusCode::UNAUTHORIZED)
+        .body(String::new())
+        .map_err(|error| warp::reject())
+}
+
+#[derive(Deserialize)]
+struct EditAccountData {
+    username: Option<String>,
+    password: Option<String>,
+}
+
+impl EditAccountData {
+    fn is_empty(&self) -> bool {
+        self.username.is_none() && self.password.is_none()
+    }
+}
+
+fn edit_account(
+    session: session::Session,
+    account_id: i32,
+    edit_data: EditAccountData,
+) -> Result<Response<String>, Rejection> {
+    if session.logged_in() {
+        if let Some(session_account) = session.account() {
+            if !edit_data.is_empty() {
+                if session_account.id == account_id {
+                    let mut errors: Vec<String> = vec![];
+                    use random_tables_web::schema::accounts::dsl::*;
+                    let update = diesel::update(accounts).filter(id.eq(account_id));
+
+                    let mut result_username = None;
+                    if let Some(new_username) = edit_data.username {
+                        result_username = Some(
+                            update
+                                .set(username.eq(new_username))
+                                .returning(username)
+                                .get_result::<String>(session.connection()),
+                        );
+                    }
+
+                    let mut result_password = None; // Option<Result<String, diesel::Error>>
+                    if let Some(new_password) = edit_data.password {
+                        let new_password = bcrypt::hash(&new_password, bcrypt::DEFAULT_COST);
+
+                        match new_password {
+                            Ok(new_password) => {
+                                result_password = Some(
+                                    update
+                                        .set(password_hash.eq(new_password))
+                                        .returning(password_hash)
+                                        .get_result::<String>(session.connection()),
+                                );
+                            }
+                            Err(error) => errors.push(format!("{}", error).to_string()),
+                        }
+                    }
+
+                    let mut result_account = data::account::Account {
+                        id: account_id,
+                        username: session_account.username.clone(),
+                    };
+
+                    result_username.and_then(|result| {
+                        let err = result.and_then(|new_username| {
+                            result_account.username = new_username;
+                            Ok(())
+                        });
+                        if let Err(err) = err {
+                            let error = format!("{}", err).to_string();
+                            errors.push(error.clone());
+                            return Some(error);
+                        }
+                        None
+                    });
+
+                    result_password.and_then(|result| {
+                        let err = result.and_then(|new_password| Ok(()));
+                        if let Err(err) = err {
+                            let error = format!("{}", err).to_string();
+                            errors.push(error.clone());
+                            return Some(error);
+                        }
+                        None
+                    });
+
+                    if errors.is_empty() {
+                        return Response::builder()
+                            .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
+                            .body(
+                                serde_json::to_string(&errors)
+                                    .unwrap_or("Failed to JSON-ify errors.".to_string()),
+                            )
+                            .map_err(|error| warp::reject());
+                    } else {
+                        return Response::builder()
+                            .status(warp::http::StatusCode::OK)
+                            .body(
+                                serde_json::to_string(&result_account).unwrap_or(
+                                    "Failed to create JSON for updated data.".to_string(),
+                                ),
+                            )
+                            .map_err(|error| warp::reject());
+                    }
+                }
             }
         }
     }
